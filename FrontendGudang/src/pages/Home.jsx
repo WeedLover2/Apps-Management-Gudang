89ad2c68694d008d108f2e86d963c8e7d92b318f
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-09-15 12:16:45 +0700

    add Home page and not found page

---
+++ FrontendGudang/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+	<div className="flex flex-col items-center justify-center min-h-[60vh]">
+		<h1 className="text-3xl font-bold mb-4">Selamat Datang di GudangApp</h1>
+		<p className="text-gray-600">Aplikasi manajemen gudang sederhana.</p>
+	</div>
+);
+
+export default Home;
