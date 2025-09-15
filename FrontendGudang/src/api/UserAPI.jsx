13e349b779581faa3220cd95376f69161a8bb714
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-09-15 11:32:34 +0700

    Integrate UserApi from fe with UserController from be

--- FrontendGudang/src/api/UserAPI.jsx
+++ FrontendGudang/src/api/UserAPI.jsx
@@ -2,3 +2,15 @@ import axios from "axios";
 
 export const BASEURL = 'http://localhost:5000';
 const API_USER = `${BASEURL}/api/users`;
+
+// Get all user
+export const getUsers = async () => {
+	const response = await axios.get(API_USER);
+	return response.data;
+};
+
+// Add new user (admin/basic user)
+export const createUser = async (userData) => {
+  const response = await axios.post(API_USER, userData);
+  return response.data;
+};
