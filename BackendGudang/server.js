47526ef4a86af88ebb30e219e3b50feeda9b7887
GrassEater2 <tiantirta@gmail.com>
2025-09-15 11:38:02 +0700

    adding swagger

--- BackendGudang/server.js
+++ BackendGudang/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 dotenv.config();
 connectDB();
@@ -13,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
+// Swagger API Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 // Routes
 const userRoutes = require('./routes/UserRoutes');
