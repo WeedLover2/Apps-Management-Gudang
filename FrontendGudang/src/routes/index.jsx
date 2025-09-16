845f7f93a13db15b1122c35262dce7b6a9ba1c3f
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-09-16 08:28:57 +0700

    Change the Route of Index.jsx to fix animation of Modal when be opened or closed

--- FrontendGudang/src/routes/index.jsx
+++ FrontendGudang/src/routes/index.jsx
@@ -1,25 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../Components/Login";
 import NotFound from "../pages/NotFound";
 import Navbar from "../Components/Navbar";
 
-// Komponen khusus agar modal login bisa close dengan navigate
 function LoginRoute() {
   const navigate = useNavigate();
   return <Login isOpen={true} onClose={() => navigate("/")} />;
 }
 
-const AppRoutes = () => (
+const AppRoutes = () => {
+  const location = useLocation();
+  const state = location.state;
+  const backgroundLocation = state && state.backgroundLocation;
+
+  return (
+    <>
+      <Navbar />
+      <Routes location={backgroundLocation || location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      {/* Modal Login */}
+      {location.pathname === "/login" && (
+        <LoginRoute />
+      )}
+    </>
+  );
+};
+
+const RouterWrapper = () => (
   <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginRoute />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <AppRoutes />
   </Router>
 );
 
-export default AppRoutes;
+export default RouterWrapper;
