e3580ae50be6232dccf761a795f7f9802cfd3943
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-09-16 12:13:31 +0700

    Add global state to save login state and add AuthProvider in main.jsx

--- FrontendGudang/src/main.jsx
+++ FrontendGudang/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RouterWrapper from './routes/index.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AuthProvider>
     <RouterWrapper />
-  </StrictMode>,
+  </AuthProvider>,
 )
