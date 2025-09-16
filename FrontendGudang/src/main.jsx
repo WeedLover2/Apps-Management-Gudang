ef8df6e109289426006b3cde942db8d45cb57f41
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-09-16 08:31:14 +0700

    Change AppRoutes into ROuteWrapper to make it easier to isolate all of routing logic

--- FrontendGudang/src/main.jsx
+++ FrontendGudang/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
-import AppRoutes from './routes/index.jsx'
+import RouterWrapper from './routes/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppRoutes />
+    <RouterWrapper />
   </StrictMode>,
 )
