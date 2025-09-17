4428b9c7149eabc4647e58187d544bb13bcf6669
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-09-17 16:30:58 +0700

    change the link to show icon on homw when nothing to show

--- FrontendGudang/src/pages/Home.jsx
+++ FrontendGudang/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { ProductContext } from "../context/ProductContext";
 const EmptyIcon = () => (
   <div className="flex flex-col items-center justify-center h-96">
     <img 
-			src="https://i.pinimgproxy.com/?url=aHR0cHM6Ly9jZG4taWNvbnMtcG5nLmZsYXRpY29uLmNvbS8yNTYvMjI5My8yMjkzNjE0LnBuZw==&ts=1758084311&sig=72d8ee1174d1c64d37a9667f7c6eca71b2ece15f9927d4d0d41d02caf4282142" 
+			src="https://i.pinimg.com/736x/6b/c2/7f/6bc27f9435b6d687fc5d0307ffbfbf82.jpg" 
 			alt="No Product" 
 			className="w-32 h-32 opacity-50 mb-4" />
     <div className="text-gray-400 text-lg">Belum ada produk</div>
